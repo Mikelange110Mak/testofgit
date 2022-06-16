43f18744a95d4dc6698a78312426c47c38043919
Mak <infinitycorpkz@gmail.com>
2022-06-16 11:53:22 +0600

    21 log operatory

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ if (4 == 9) {
 }
 */
 
-const num = 50;
+//const num = 50;
 
 /*
 if (num < 49) {
@@ -45,6 +45,8 @@ if (num < 49) {
 */
 
 //(num === 50) ? console.log('Right!') : console.log('Errorushka(');;
+
+/*
 
 switch (num) {
    case 49:
@@ -59,4 +61,80 @@ switch (num) {
    default:
       console.log('Расслабься ковбой!');
       break
+}
+*/
+
+/////Логические операторы 21//
+/*
+const hamburger = 5;
+const fries = 'netu';
+
+if (hamburger && fries) {
+   console.log('im not hungry');
+} 
+*/
+/*
+const hamburger = 2;
+const fries = 1;
+
+if (hamburger === 3 && fries) {
+   console.log('everybody arent hungry')
+} else {
+   console.log('blya ne pohavali');
+}
+*/
+
+//Результат будет выдаваться когда оператор наткнется на не правду (0, false итд), а если все правильно, то последнее значение
+
+/*
+const hamburger = 0;
+const fries = 0;
+const cola = 0;
+
+if (hamburger || fries || fries) {
+   console.log('everybody are happy')
+} else {
+   console.log('blya ne pohavali');
+}
+console.log(hamburger || fries || fries);
+*/
+
+/*
+let johnReport, kairatReport, bekzatReport, saniyaReport = 'done';
+console.log(johnReport || kairatReport || bekzatReport || saniyaReport);
+*/
+
+/*
+const hamburger = 0;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+   console.log('everybody are happy')
+} else {
+   console.log('blya ne pohavali');
+}
+console.log(hamburger || fries || fries);
+*/
+console.log(NaN || 2 || undefined);
+console.log(NaN && 2 && undefined);
+console.log(1 && 2 && 3);
+console.log(!1 && 2 || !3);
+console.log(25 || null && !3);
+console.log(NaN || null || !3 || undefined || 5);
+console.log(NaN || null && !3 && undefined || 5);
+console.log(5 === 5 && 3 > 1 || 5);
+
+
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+} else {
+   console.log('no');
 }
