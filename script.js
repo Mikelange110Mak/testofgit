80af220acb9c636014ebc61a6d68c81acf677a29
Mak <infinitycorpkz@gmail.com>
2022-06-16 10:53:12 +0600

    usloviya

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*
+
 //1
 const numberOfFilms = +prompt('Сколько фильмов чекнул?', '');
 
@@ -19,3 +21,42 @@ const a = prompt('Последний просмотренный фильм?'),
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 console.log(personalMovieDB);
+
+*/
+
+/*
+if (4 == 9) {
+   console.log('OK!');
+} else {
+   console.log('Errorchik');
+}
+*/
+
+const num = 50;
+
+/*
+if (num < 49) {
+   console.log('Error');
+} else if (num > 100) {
+   console.log('Mnogo');
+} else {
+   console.log('Ok1');
+}
+*/
+
+//(num === 50) ? console.log('Right!') : console.log('Errorushka(');;
+
+switch (num) {
+   case 49:
+      console.log('not right');
+      break;
+   case 100:
+      console.log('not right');
+      break
+   case 50:
+      console.log('at the point!');
+      break
+   default:
+      console.log('Расслабься ковбой!');
+      break
+}
